b85914cae506dca39a99fe0283dedc5c476359cf
Eric Nelson <ernelson12o0@gmail.com>
2023-08-31 16:24:11 -0400

    Lab Complete

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,47 @@
 #!/usr/bin/env python3
+import re
 
 class MyString:
-  pass
+    def __init__(self, value = ''):
+        if  isinstance(value, str) and value != '':
+            self.value = value
+        
+        else:
+            print('The value must be a string.')
+
+    def is_sentence(self):
+        if(self.value.endswith('.') == True):
+            return True
+        
+        else:
+            return False
+        
+    def is_question(self):
+        if(self.value.endswith('?') == True):
+            return True
+        
+        else:
+            return False
+        
+    def is_exclamation(self):
+        if(self.value.endswith('!') == True):
+            return True
+        
+        else:
+            return False
+        
+    def count_sentences(self):
+        if hasattr(self, "value"):
+            cleaned_value = self.value.replace(',', '')
+
+            sentences = re.split(r'[.!?]', cleaned_value)
+    
+            sentences = [sentence.strip() for sentence in sentences if sentence.strip()]
+
+            length = len(sentences)
+            return length
+        else:
+            return 0
+            
+        
+            
